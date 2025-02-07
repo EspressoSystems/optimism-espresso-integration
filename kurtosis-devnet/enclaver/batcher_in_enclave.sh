7f23ff5a8b72246c413e13a25b36628efa2caf98
EC2 Default User <ec2-user@ip-172-31-37-114.us-east-2.compute.internal>
2025-02-07 22:58:43 +0000

    more comment

--- kurtosis-devnet/enclaver/batcher_in_enclave.sh
+++ kurtosis-devnet/enclaver/batcher_in_enclave.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+# Build the docker image
 docker build --no-cache -t op-batcher:app -f kurtosis-devnet/enclaver/Dockerfile .
+
+# Build the enclave
 sudo enclaver build --file kurtosis-devnet/enclaver/enclaver.yaml
+
+# Run the enclave
 sudo enclaver run enclave-batcher:latest
