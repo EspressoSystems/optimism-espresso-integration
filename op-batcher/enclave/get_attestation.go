4325b80c3709382d4b96eefe4fc468aedb743a0a
dailinsubjam <dlsubjam@gmail.com>
2025-02-22 09:49:25 +0000

    initial commit on generating attestation

--- op-batcher/enclave/get_attestation.go
+++ op-batcher/enclave/get_attestation.go
@@ -1,45 +1,47 @@
 package enclave
 
 import (
+	"crypto/ecdsa"
 	"errors"
 
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/hf/nsm"
 	"github.com/hf/nsm/request"
 )
 
 func attest(nonce, userData, publicKey []byte) ([]byte, error) {
 	sess, err := nsm.OpenDefaultSession()
-	defer sess.Close()
-
-	if nil != err {
+	if err != nil {
 		return nil, err
 	}
+	defer sess.Close()
 
 	res, err := sess.Send(&request.Attestation{
 		Nonce:     nonce,
 		UserData:  userData,
 		PublicKey: publicKey,
 	})
-	if nil != err {
+	if err != nil {
 		return nil, err
 	}
 
-	if "" != res.Error {
+	if res.Error != "" {
 		return nil, errors.New(string(res.Error))
 	}
 
-	if nil == res.Attestation || nil == res.Attestation.Document {
+	if res.Attestation == nil || res.Attestation.Document == nil {
 		return nil, errors.New("NSM device did not return an attestation")
 	}
 
 	return res.Attestation.Document, nil
 }
 
-func GetAttestationWithTxData(txData []byte) ([]byte, error) {
+func GetAttestationWithTxData(publicKey *ecdsa.PublicKey) ([]byte, error) {
 	// Use empty slices for nonce and publicKey when they're not needed
 	nonce := make([]byte, 0)
-	publicKey := make([]byte, 0)
+	txData := make([]byte, 0)
+	publicKeyBytes := crypto.FromECDSAPub(publicKey)
 
 	// Call the existing attest function with txData as userData
-	return attest(nonce, txData, publicKey)
+	return attest(nonce, txData, publicKeyBytes)
 }
