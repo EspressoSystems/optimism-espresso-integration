2a8a269b4664aa8a94f16ae629468170c6f63d61
Theodore Schnepper <tschnepper@espressosys.com>
2025-03-05 11:55:38 -0700

    Add Batcher Signature to Espresso transaction
    In order to be able to verify that a transaction coming from Espresso's finalized
    transaction list was originally sourced from the op-batcher, we need a way to
    verify that the transaction did actually come from the op-batcher.  To facilitate
    this we want the transaction submitted to espresso to be signed by the op-batcher
    itself.
    
    The majority of this change is a result of attempting to forward the signing
    functions to the place where we need them, while also maintaining no ability to
    have direct memory access to any potentially stored private key.
    
    This change signs the payload with the desired private key before it is submitted
    to Espresso.
    
    Update comments on `clientSigner` and `privateKeySigner`
    
    The comment on `privateKeySigner` is for different code altogether.  It should
    be replaced with a comment that applies to `privateKeySigner` itself.  Additionally
    the comments could use some more explicit purpose.
    
    This change updates the comments on `privateKeySigner` and `clientSigner` that
    help to      explain what they are intended for.
    
    Fix ChainSigner not being set for Config and DriverSetup
    
    The ChainSigner is created, but it's never assigned to Config and for DriverSetup.
    This leads to null pointer calls when trying to sign the payload for the chain.
    
    Clean up comment that had an extra "creates a"

--- op-batcher/batcher/service.go
+++ op-batcher/batcher/service.go
@@ -12,6 +12,7 @@ import (
 
 	espresso "github.com/EspressoSystems/espresso-sequencer-go/client"
 	espressoLightClient "github.com/EspressoSystems/espresso-sequencer-go/light-client"
+	opcrypto "github.com/ethereum-optimism/optimism/op-service/crypto"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -75,6 +76,7 @@ type BatcherService struct {
 	EspressoLightClient *espressoLightClient.LightClientReader
 
 	BatcherConfig
+	opcrypto.ChainSigner
 
 	ChannelConfig ChannelConfigProvider
 	RollupConfig  *rollup.Config
@@ -376,6 +378,14 @@ func (bs *BatcherService) initTxManager(cfg *CLIConfig) error {
 		return err
 	}
 	bs.TxManager = txManager
+
+	// We want to be able to access the signer
+	cast, castOk := bs.TxManager.(opcrypto.ChainSigner)
+	if !castOk {
+		return fmt.Errorf("tx manager does not implement ChainSigner")
+	}
+	bs.ChainSigner = cast
+
 	return nil
 }
 
@@ -421,6 +431,7 @@ func (bs *BatcherService) initDriver(opts ...DriverSetupOption) {
 		Metr:                bs.Metrics,
 		RollupConfig:        bs.RollupConfig,
 		Config:              bs.BatcherConfig,
+		ChainSigner:         bs.ChainSigner,
 		Txmgr:               bs.TxManager,
 		L1Client:            bs.L1Client,
 		EndpointProvider:    bs.EndpointProvider,
