cb3dd6dd46f334779967f46340edf9067b680a5c
Philippe Camacho <philippe@espressosys.com>
2025-03-28 09:13:28 -0300

    Download espresso go library with nix

--- flake.nix
+++ flake.nix
@@ -13,6 +13,10 @@
         inputs.foundry.overlay
       ];
       pkgs = import inputs.nixpkgs { inherit overlays system;};
+      downloadedFile = pkgs.fetchurl {
+              url = "https://github.com/EspressoSystems/espresso-network-go/releases/download/v0.0.34/libespresso_crypto_helper-x86_64-unknown-linux-gnu.a";
+              sha256 = "sha256:1c7ybrqjrp1709j08fk7zcr5q8hyfakvgv0m64zn2fywlqfdpszs";
+            };
       in
       {
         devShell = pkgs.mkShell {
@@ -27,6 +31,12 @@
             pkgs.go_1_22
             pkgs.gotools
           ];
+          shellHook = ''
+                    export DOWNLOADED_FILE_PATH=${downloadedFile}
+                    echo "Downloaded file is at $DOWNLOADED_FILE_PATH"
+                    ln -sf /nix/store/8b5ranvnlb7sjrzpdpbb75vdp0gsyb1x-libespresso_crypto_helper-x86_64-unknown-linux-gnu.a /tmp/libespresso_crypto_helper-x86_64-unknown-linux-gnu.a
+                    export CGO_LDFLAGS="-L/tmp -lespresso_crypto_helper-x86_64-unknown-linux-gnu"
+                  '';
         };
       }
     );
