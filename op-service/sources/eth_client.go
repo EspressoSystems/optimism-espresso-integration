509652861ad0e2ae8945676d84f1f02a2de2fc49
Sishan Long <dlsubjam@gmail.com>
2025-04-03 14:16:55 -0700

    Caff Node only allow finalized L1 Origin

--- op-service/sources/eth_client.go
+++ op-service/sources/eth_client.go
@@ -603,3 +603,13 @@ func (s *EthClient) NewMultiCaller(batchSize int) *batching.MultiCaller {
 func (s *EthClient) RPC() client.RPC {
 	return s.client
 }
+func (s *EthClient) FinalizedBlock() (eth.L1BlockRef, error) {
+	var block *RPCBlock
+	err := s.client.CallContext(context.Background(), &block, "eth_getBlockByNumber", "finalized", false)
+	if err != nil {
+		return eth.L1BlockRef{}, fmt.Errorf("failed to fetch finalized block: %w", err)
+	}
+
+	num := uint64(block.Number)
+	return s.BlockRefByNumber(context.Background(), num)
+}
