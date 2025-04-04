6d7efac347f77ed8626317e9654c5cdbd2bd6793
Theodore Schnepper <Ayiga@MSN.com>
2025-04-04 05:59:35 -0600

    Add utilities and helpers to quickly spin up a test environment with `espresso-dev-node`

---
+++ espresso/environment/e2e_helpers.go
@@ -0,0 +1,52 @@
+package environment
+
+import (
+	"math/big"
+
+	"github.com/ethereum-optimism/optimism/op-e2e/system/helpers"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+// L2TxWithAmount is a helper.TxOptsFn that sets the Amount of the transaction.
+func L2TxWithAmount(amount *big.Int) helpers.TxOptsFn {
+	return func(opts *helpers.TxOpts) {
+		opts.Value = amount
+	}
+}
+
+// L2TxWithNonce is a helper.TxOptsFn that sets the Nonce of the transaction.
+func L2TxWithNonce(nonce uint64) helpers.TxOptsFn {
+	return func(opts *helpers.TxOpts) {
+		opts.Nonce = nonce
+	}
+}
+
+// L2WithToAddress is a helper.TxOptsFn that sets the To address of the
+// transaction.
+func L2TxWithToAddress(toAddr *common.Address) helpers.TxOptsFn {
+	return func(opts *helpers.TxOpts) {
+		opts.ToAddr = toAddr
+	}
+}
+
+// L2TxWithVerifyOnClients is a helper.TxOptsFn that sets the list of
+// verification clients to verify the transaction on.
+func L2TxWithVerifyOnClients(clients ...*ethclient.Client) helpers.TxOptsFn {
+	return func(opts *helpers.TxOpts) {
+		opts.VerifyOnClients(clients...)
+	}
+}
+
+// L2TxWithOptions is a helper.TxOptsFn that sets multiple options for the
+// transaction. By default the L2 transaction helper function is only able
+// to accept a single helpers.TxOptsFn, so this function allows multiple
+// to be passed as a single option, allowing for more granular configuration
+// options.
+func L2TxWithOptions(options ...helpers.TxOptsFn) helpers.TxOptsFn {
+	return func(opts *helpers.TxOpts) {
+		for _, option := range options {
+			option(opts)
+		}
+	}
+}
