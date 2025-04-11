4c82bdcf7c7ea0091900071aed086069674f9119
Keyao Shen <shenkeyao@gmail.com>
2025-04-11 10:17:24 -0700

    Remove code related to Light Client contract

--- op-batcher/batcher/config.go
+++ op-batcher/batcher/config.go
@@ -125,8 +125,7 @@ type CLIConfig struct {
 	RPC           oprpc.CLIConfig
 	AltDA         altda.CLIConfig
 
-	EspressoUrl             string
-	EspressoLightClientAddr string
+	EspressoUrl string
 }
 
 func (c *CLIConfig) Check() error {
@@ -225,6 +224,5 @@ func NewConfig(ctx *cli.Context) *CLIConfig {
 		ThrottleAlwaysBlockSize:       ctx.Uint64(flags.ThrottleAlwaysBlockSizeFlag.Name),
 		AdditionalThrottlingEndpoints: ctx.StringSlice(flags.AdditionalThrottlingEndpointsFlag.Name),
 		EspressoUrl:                   ctx.String(flags.EspressoUrlFlag.Name),
-		EspressoLightClientAddr:       ctx.String(flags.EspressoLCAddrFlag.Name),
 	}
 }
