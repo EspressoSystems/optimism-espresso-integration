36d2b73cf722bbbd39fb6b650870a5a4e052fa17
Philippe Camacho <philippe@espressosys.com>
2025-04-14 16:29:11 -0400

    Display Espresso go library version when entering nix shell.

--- flake.nix
+++ flake.nix
@@ -12,20 +12,21 @@
       overlays = [
         inputs.foundry.overlay
       ];
+      espresso_go_lib_version = "v0.0.35";
       pkgs = import inputs.nixpkgs { inherit overlays system;};
       espressoGoLibFile = if system == "x86_64-linux"
                 then pkgs.fetchurl {
-                  url = "https://github.com/EspressoSystems/espresso-network-go/releases/download/v0.0.35/libespresso_crypto_helper-x86_64-unknown-linux-gnu.a";
+                  url = "https://github.com/EspressoSystems/espresso-network-go/releases/download/${espresso_go_lib_version}/libespresso_crypto_helper-x86_64-unknown-linux-gnu.a";
                   sha256 = "sha256:07yfsrphfpq7w40x2rnldswzzbd4j0p5jdmm74132cqbf02pn8y8";
                 }
                 else if system == "x86_64-darwin" then
                   pkgs.fetchurl {
-                    url = "https://github.com/EspressoSystems/espresso-network-go/releases/download/v0.0.35/libespresso_crypto_helper-x86_64-apple-darwin.a";
+                    url = "https://github.com/EspressoSystems/espresso-network-go/releases/download/${espresso_go_lib_version}/libespresso_crypto_helper-x86_64-apple-darwin.a";
                     sha256 = "sha256:1va49y81p3yrf9z61srw6rfysmbbk2vix0r7l8i2mz8b3ln0gsgy";
                   }
                 else # aarch64-darwin
                   pkgs.fetchurl {
-                        url = "https://github.com/EspressoSystems/espresso-network-go/releases/download/v0.0.35/libespresso_crypto_helper-aarch64-apple-darwin.a";
+                        url = "https://github.com/EspressoSystems/espresso-network-go/releases/download/${espresso_go_lib_version}/libespresso_crypto_helper-aarch64-apple-darwin.a";
                         sha256 = "sha256:1fp0v9d3b41lkfpva6rz35xi832xq4355pw5785ym2jm69pcsnnn";
                   }
                   ;
@@ -57,7 +58,7 @@
           shellHook = ''
           export FOUNDRY_DISABLE_NIGHTLY_WARNING=1
           export DOWNLOADED_FILE_PATH=${espressoGoLibFile}
-          echo "Espresso go library stored at $DOWNLOADED_FILE_PATH"
+          echo "Espresso go library ${espresso_go_lib_version} stored at $DOWNLOADED_FILE_PATH"
           ln -sf ${espressoGoLibFile} ${target_link}
           export CGO_LDFLAGS="${cgo_ld_flags}"
                   '';
