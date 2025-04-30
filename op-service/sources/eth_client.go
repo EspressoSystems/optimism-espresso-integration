ea03aef2645e59bd273c12f459a6b9c1e835cfd1
Sishan Long <dlsubjam@gmail.com>
2025-04-30 15:54:03 -0700

    Add caff node L1 finality check

--- op-service/sources/eth_client.go
+++ op-service/sources/eth_client.go
@@ -605,7 +605,7 @@ func (s *EthClient) RPC() client.RPC {
 }
 func (s *EthClient) FinalizedBlock() (eth.L1BlockRef, error) {
 	var block *RPCBlock
-	err := s.client.CallContext(context.Background(), &block, "eth_getBlockByNumber", "finalized", false)
+	err := s.client.CallContext(context.Background(), &block, "eth_getBlockByNumber", "finalized", true)
 	if err != nil {
 		return eth.L1BlockRef{}, fmt.Errorf("failed to fetch finalized block: %w", err)
 	}
