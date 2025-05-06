cc5fec1214db9f359fd0694a7431c694bb622ddc
Keyao Shen <shenkeyao@gmail.com>
2025-05-06 13:07:40 -0700

    Increase WithL1FinalizedDistance for the test

--- espresso/environment/8_reorg_test.go
+++ espresso/environment/8_reorg_test.go
@@ -31,7 +31,7 @@ func TestBatcherWaitForFinality(t *testing.T) {
 
 	// Set NonFinalizedProposals to true and SequencerUseFinalized to false, to make sure we are
 	// testing how the batcher handles the finality.
-	system, espressoDevNode, err := launcher.StartDevNet(ctx, t, env.WithL1FinalizedDistance(0), env.WithNonFinalizedProposals(true), env.WithSequencerUseFinalized(false))
+	system, espressoDevNode, err := launcher.StartDevNet(ctx, t, env.WithL1FinalizedDistance(4), env.WithNonFinalizedProposals(true), env.WithSequencerUseFinalized(false))
 	if have, want := err, error(nil); have != want {
 		t.Fatalf("failed to start dev environment with espresso dev node:\nhave:\n\t\"%v\"\nwant:\n\t\"%v\"\n", have, want)
 	}
