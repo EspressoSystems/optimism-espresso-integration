7d2d7e2ec784f8017fabcabb312d5dd3802da7d4
Phil <philippe@espressosys.com>
2025-05-07 17:07:51 -0400

    Faster integration test 7 (#122)
    * New instance of the streamer when restarting the batcher.
    
    * Pinpoint espresso dev node docker image to release-color-colorful-snake.
    
    * Wait for the next safe L2 block to make the test more robust.

--- op-batcher/batcher/driver.go
+++ op-batcher/batcher/driver.go
@@ -160,6 +160,7 @@ func NewBatchSubmitter(setup DriverSetup) *BatchSubmitter {
 		},
 		2*time.Second,
 	)
+
 	log.Info("Streamer started", "streamer", batchSubmitter.streamer)
 
 	return batchSubmitter
@@ -209,6 +210,7 @@ func (l *BatchSubmitter) StartBatchSubmitting() error {
 	}
 
 	if l.Config.UseEspresso {
+
 		err := l.registerBatcher(l.killCtx)
 		if err != nil {
 			return fmt.Errorf("could not register with batch inbox contract: %w", err)
