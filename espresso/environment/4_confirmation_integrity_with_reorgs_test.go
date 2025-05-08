bc0d44fd83c8b2b503308b58d53c60f05fe2d3eb
Philippe Camacho <philippe@espressosys.com>
2025-05-08 09:13:25 -0400

    Introduce BatchHash function. Test passing.

--- espresso/environment/4_confirmation_integrity_with_reorgs_test.go
+++ espresso/environment/4_confirmation_integrity_with_reorgs_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"math/big"
+	"slices"
 	"testing"
 	"time"
 )
@@ -25,17 +26,25 @@ import (
 //	Act:
 //		Store the (unfinalized) head of the L1 in variable h.
 //		Wait for the first n batches to be posted on possibly unfinalized L1 blocks.
-//		Collect the L2 block hashes of the corresponding batches and store them in list L.
+//		Collect the batches of the corresponding batches and store them in list L.
 //		Reorg the L1 to height h.
 //		Wait for the L2 to reach safe height n again as the corresponding batches are submitted again to L1.
-//		Store these n L2 blocks in L'
+//		Store these n batches in L'
 //	Assert:
 //		L == L'
 
-func getBatchesPublishedOnUnfinalizedL1Blocks(ctx context.Context, t *testing.T, system *e2esys.System, l1Client *ethclient.Client, l2Seq *ethclient.Client, l2Verif *ethclient.Client) ([]string, uint64) {
-	var l2HeadHashes []string
+// TODO function to compute the hash of a batch
+func BatchHash(b *derive.SingularBatch) string {
+	hashes := ""
+	for _, tx := range b.Transactions {
+		hashes = hashes + tx.String()
+	}
 
-	l2HeadL1Info := &derive.L1BlockInfo{}
+	return hashes
+}
+
+func getBatchesPublishedOnUnfinalizedL1Blocks(ctx context.Context, t *testing.T, system *e2esys.System, l1Client *ethclient.Client, l2Seq *ethclient.Client, l2Verif *ethclient.Client) ([]string, uint64, uint64) {
+	var batchInfo []string
 
 	l1Height, err := l1Client.BlockNumber(ctx)
 	require.NoError(t, err)
@@ -45,51 +54,54 @@ func getBatchesPublishedOnUnfinalizedL1Blocks(ctx context.Context, t *testing.T,
 	i := int64(0)
 	// Fetch height of the most recent block which is unsafe and which batch will be sent to L1 at a later stage
 	unsafeL2BlockNumber, err := l2Seq.BlockNumber(ctx)
+
 	require.NoError(t, err)
 	for (l1Height - l1HeightStart) < system.Cfg.L1FinalizedDistance {
 		height := uint64(i) + unsafeL2BlockNumber
 
 		l2Head, err := l2Seq.BlockByNumber(ctx, new(big.Int).SetUint64(height))
-		time.Sleep(1 * time.Second)
+		time.Sleep(500 * time.Millisecond)
 		if err != nil {
 			continue
 		} else {
 			i++
 		}
-
-		hash := l2Head.Hash().String()
-		t.Log("New element", "value", hash, "list length", len(l2HeadHashes))
-		l2HeadHashes = append(l2HeadHashes, hash)
-
-		_, l2HeadL1Info, err = derive.BlockToSingularBatch(system.RollupCfg(), l2Head)
+		batch, l2HeadL1Info, err := derive.BlockToSingularBatch(system.RollupCfg(), l2Head)
 		log.Info("l2HeadL1Info", "value", l2HeadL1Info)
+
+		batchHash := BatchHash(batch)
+		t.Log("New element", "value", batchHash, "list length", len(batchInfo))
+		// Insert new elements only
+		if !slices.Contains(batchInfo, batchHash) {
+			batchInfo = append(batchInfo, batchHash)
+		}
 
 		l1Height, err = l1Client.BlockNumber(ctx)
 		require.NoError(t, err)
 
 	}
 
-	// Wait for blocks to be safe TODO on l2Verif
-
-	return l2HeadHashes, l1HeightStart
+	return batchInfo, l1HeightStart, unsafeL2BlockNumber
 }
 
-func getFirstNL2SafeBlocks(ctx context.Context, t *testing.T, l2Verif *ethclient.Client, n int) []string {
-	var l2HeadHashes []string
+func getFirstNL2SafeBlocks(ctx context.Context, t *testing.T, system *e2esys.System, l2Verif *ethclient.Client, n int, startIndex uint64) []string {
+	var batches []string
 
 	for i := 0; i < n; i++ {
-		height := uint64(i)
+		height := startIndex + uint64(i)
 		_, err := geth.WaitForBlockToBeSafe(big.NewInt(int64(height)), l2Verif, 2*time.Minute)
 		require.NoError(t, err)
 
 		l2Head, err := l2Verif.BlockByNumber(ctx, new(big.Int).SetUint64(height))
 		require.NoError(t, err)
 
-		hash := l2Head.Hash().String()
-		l2HeadHashes = append(l2HeadHashes, hash)
+		//hash := l2Head.Hash().String()
+		batch, _, err := derive.BlockToSingularBatch(system.RollupCfg(), l2Head)
+		batchHash := BatchHash(batch)
+		batches = append(batches, batchHash)
 
 	}
-	return l2HeadHashes
+	return batches
 }
 
 func run(ctx context.Context, t *testing.T, system *e2esys.System) {
@@ -109,7 +121,7 @@ func run(ctx context.Context, t *testing.T, system *e2esys.System) {
 	t.Log("L2 is progressing")
 
 	// Fetch batches before reorg
-	batchesBefore, L1BlockHeightToReorgTo := getBatchesPublishedOnUnfinalizedL1Blocks(ctx, t, system, l1Client, l2Seq, l2Verif)
+	batchesBefore, L1BlockHeightToReorgTo, startIndex := getBatchesPublishedOnUnfinalizedL1Blocks(ctx, t, system, l1Client, l2Seq, l2Verif)
 
 	l1Origin, err := l1Client.BlockByNumber(ctx, new(big.Int).SetUint64(L1BlockHeightToReorgTo))
 	require.NoError(t, err)
@@ -123,7 +135,7 @@ func run(ctx context.Context, t *testing.T, system *e2esys.System) {
 	require.NoError(t, err)
 
 	n := len(batchesBefore)
-	batchesAfter := getFirstNL2SafeBlocks(ctx, t, l2Verif, n)
+	batchesAfter := getFirstNL2SafeBlocks(ctx, t, system, l2Verif, n, startIndex)
 
 	log.Info("+++ L2 blocks after reorg", "value", batchesAfter)
 
