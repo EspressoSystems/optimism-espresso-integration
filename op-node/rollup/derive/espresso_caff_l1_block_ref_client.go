950fc85ba2921fa242daf02529e54de43700e244
Sishan Long <dlsubjam@gmail.com>
2025-05-13 16:20:18 -0700

    Simplify Caff Node L1 Finalized Block Fetcher Logic

--- op-node/rollup/derive/espresso_caff_l1_block_ref_client.go
+++ op-node/rollup/derive/espresso_caff_l1_block_ref_client.go
@@ -4,27 +4,24 @@ import (
 	"context"
 	"math/big"
 
-	"github.com/ethereum-optimism/optimism/op-service/eth"
 	"github.com/ethereum/go-ethereum/common"
 )
 
 // L1BlockRefClient is a wrapper around eth.L1BlockRef that implements the espresso.L1Client interface
 type L1BlockRefClient struct {
-	L1FinalizedBlock   func() (eth.L1BlockRef, error)
-	L1BlockRefByNumber func(ctx context.Context, num uint64) (eth.L1BlockRef, error)
+	L1Fetcher L1Fetcher
 }
 
 // NewL1BlockRefClient creates a new L1BlockRefClient
-func NewL1BlockRefClient(L1FinalizedBlock func() (eth.L1BlockRef, error), L1BlockRefByNumber func(ctx context.Context, num uint64) (eth.L1BlockRef, error)) *L1BlockRefClient {
+func NewL1BlockRefClient(L1Fetcher L1Fetcher) *L1BlockRefClient {
 	return &L1BlockRefClient{
-		L1FinalizedBlock:   L1FinalizedBlock,
-		L1BlockRefByNumber: L1BlockRefByNumber,
+		L1Fetcher: L1Fetcher,
 	}
 }
 
 // HeaderHashByNumber implements the espresso.L1Client interface
 func (c *L1BlockRefClient) HeaderHashByNumber(ctx context.Context, number *big.Int) (common.Hash, error) {
-	expectedL1BlockRef, err := c.L1BlockRefByNumber(ctx, number.Uint64())
+	expectedL1BlockRef, err := c.L1Fetcher.L1BlockRefByNumber(ctx, number.Uint64())
 	if err != nil {
 		return common.Hash{}, err
 	}
