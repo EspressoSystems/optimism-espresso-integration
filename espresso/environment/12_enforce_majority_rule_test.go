31d81675339a8e02716c53e8805e85c58ba88e2c
Philippe Camacho <philippe@espressosys.com>
2025-05-16 11:36:50 -0400

    Use httptest library to spin up a test server.

--- espresso/environment/12_enforce_majority_rule_test.go
+++ espresso/environment/12_enforce_majority_rule_test.go
@@ -2,52 +2,19 @@ package environment_test
 
 import (
 	"context"
-	"fmt"
 	env "github.com/ethereum-optimism/optimism/espresso/environment"
 	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/geth"
 	"github.com/ethereum-optimism/optimism/op-e2e/system/e2esys"
 	"github.com/stretchr/testify/require"
 	"math/big"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 )
 
-const DUMMY_SERVER_HOST = "localhost"
-const DUMMY_SERVER_PORT = "8888"
 const ERROR_EXPECTED = true
 const NO_ERROR_EXPECTED = false
-
-// This dummy Espresso Query Service node returns "hello" to all requests
-func startServer() {
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello")
-	})
-	go func() {
-		// Optional: handle error from ListenAndServe
-		addr := fmt.Sprintf(":%s", DUMMY_SERVER_PORT)
-		if err := http.ListenAndServe(addr, handler); err != nil {
-			fmt.Println("Server error:", err)
-		}
-	}()
-}
-
-// A small test to ensure the dummy server runs properly
-func TestHelloServer(t *testing.T) {
-	startServer()
-	time.Sleep(100 * time.Millisecond) // Let server start
-
-	url := fmt.Sprintf("http://%s:%s", DUMMY_SERVER_HOST, DUMMY_SERVER_PORT)
-	resp, err := http.Get(url)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		t.Fatalf("expected status 200, got %d", resp.StatusCode)
-	}
-}
 
 // runWithMultiClient spins up the sequencer, L2 verifier and batcher in Espresso mode.
 // Moreover, a dummy Espresso Query Service (EQS) is run on port DUMMY_SERVER_PORT.
@@ -57,9 +24,11 @@ func TestHelloServer(t *testing.T) {
 // @param expectedError if set to true, we expect a timeout error as the L2 cannot make progress. Otherwise, we expect no error at all.
 func runWithMultiClient(t *testing.T, numGoodUrls int, numBadUrls int, expectedError bool) {
 
-	badServerUrl := fmt.Sprintf("http://%s:%s", DUMMY_SERVER_HOST, DUMMY_SERVER_PORT)
-	startServer()
-	time.Sleep(100 * time.Millisecond) // Let server start
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Hello", http.StatusOK)
+	}))
+
+	badServerUrl := server.URL
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
