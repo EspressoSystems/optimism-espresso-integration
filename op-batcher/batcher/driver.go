1aa61a26f9aede3a3b32d98586a150f70c5c4d94
Sishan Long <dlsubjam@gmail.com>
2025-05-19 15:43:44 -0700

    Test 3.2.2: Invalid Transaction

--- op-batcher/batcher/driver.go
+++ op-batcher/batcher/driver.go
@@ -143,6 +143,11 @@ type BatchSubmitter struct {
 	publishSignal chan pubInfo
 }
 
+// EspressoStreamer returns the batch submitter's Espresso streamer instance
+func (l *BatchSubmitter) EspressoStreamer() *espresso.EspressoStreamer[derive.EspressoBatch] {
+	return &l.streamer
+}
+
 // NewBatchSubmitter initializes the BatchSubmitter driver from a preconfigured DriverSetup
 func NewBatchSubmitter(setup DriverSetup) *BatchSubmitter {
 	state := NewChannelManager(setup.Log, setup.Metr, setup.ChannelConfig, setup.RollupConfig)
