c2e324ea82310061f87ee07f41a68955ec5ec9c8
dailinsubjam <dlsubjam@gmail.com>
2025-05-20 23:59:37 +0000

    a smallest workable version

--- espresso/environment/11_forced_transaction_test.go
+++ espresso/environment/11_forced_transaction_test.go
@@ -187,58 +187,35 @@ func ForcedWithdrawal(t *testing.T, withSmallSequencerWindow bool, withEspresso 
 	}
 
 	// Set up Alice's address and record the initial balance.
-	l2Verif := system.NodeClient(e2esys.RoleVerif)
+	// l2Verif := system.NodeClient(e2esys.RoleVerif)
 	address := system.Cfg.Secrets.Addresses().Alice
 	l1Client := system.NodeClient(e2esys.RoleL1)
-	initialBalance, err := l2Verif.BalanceAt(ctx, address, nil)
+	initialBalance, err := l1Client.BalanceAt(ctx, address, nil)
 	require.NoError(t, err, "Failed to get initial balance")
 
 	// Simulate sequencer downtime.
-	// err = system.RollupNodes["sequencer"].Stop(ctx)
-	// require.NoError(t, err, "Failed to stop sequencer")
+	err = system.RollupNodes["sequencer"].Stop(ctx)
+	require.NoError(t, err, "Failed to stop sequencer")
 
 	// Send a withdrawal from Alice to L2CrossDomainMessenger.
 	opts, err := bind.NewKeyedTransactorWithChainID(system.Cfg.Secrets.Alice, system.Cfg.L1ChainIDBig())
 	require.NoError(t, err)
-	l2CrossDomainMessengerAddr := common.HexToAddress(predeploys.L2CrossDomainMessenger)
-	// gasPrice, err := l2Verif.SuggestGasPrice(ctx)
-	// require.NoError(t, err, "Failed to get gas price")
-	// gasTipCap, err := l2Verif.SuggestGasTipCap(ctx)
-	// require.NoError(t, err, "Failed to get gas tip cap")
-	// initialNonce, err := l2Verif.PendingNonceAt(ctx, address)
-	// require.NoError(t, err, "Failed to get initial nonce")
+
 	withdrawalAmount := new(big.Int).SetUint64(1000)
-	// msgCalldata := helpers.EncodeWithdrawalCalldata(system.Cfg, address, withdrawalAmount)
 
 	portal, err := bindings.NewOptimismPortal(system.Cfg.L1Deployments.OptimismPortalProxy, l1Client)
 	require.NoError(t, err)
 	tx, err := portal.DepositTransaction(
-		opts,                       // keyed transactor for gas & value
-		l2CrossDomainMessengerAddr, // L2CrossDomainMessenger predeploy
-		withdrawalAmount,           // no ETH value needed
-		uint64(200_000),            // gas limit
-		false,                      // _isCreation
-		nil,
+		opts, // keyed transactor for gas & value
+		common.HexToAddress(predeploys.L2ToL1MessagePasser), // L2CrossDomainMessenger predeploy
+		withdrawalAmount, // no ETH value needed
+		uint64(300_000),  // gas limit
+		false,            // _isCreation
+		nil,              // _extraData
 	)
 	require.NoError(t, err)
 	_, err = bind.WaitMined(ctx, l1Client, tx)
 	require.NoError(t, err)
-
-	// txData := &types.DynamicFeeTx{
-	// 	ChainID:   system.Cfg.L2ChainIDBig(),
-	// 	Nonce:     initialNonce,
-	// 	To:        &l2CrossDomainMessengerAddr,
-	// 	Value:     withdrawalAmount,
-	// 	Gas:       500000,
-	// 	GasFeeCap: gasPrice,
-	// 	GasTipCap: gasTipCap,
-	// 	Data:      nil,
-	// }
-	// tx := types.NewTx(txData)
-	// signedTx, err := types.SignTx(tx, types.LatestSignerForChainID(system.Cfg.L2ChainIDBig()), alicePrivateKey)
-	// require.NoError(t, err, "Failed to sign withdrawal transaction")
-	// err = l2Verif.SendTransaction(ctx, signedTx)
-	// require.NoError(t, err, "Failed to send withdrawal transaction")
 
 	time.Sleep(WAIT_FORCED_TXN_TIME)
 
@@ -249,7 +226,7 @@ func ForcedWithdrawal(t *testing.T, withSmallSequencerWindow bool, withEspresso 
 	// require.NoError(t, err, "Failed to get new nonce")
 	// require.Greater(t, newNonce, initialNonce)
 
-	newBalance, err := wait.ForBalanceChange(ctx, l2Verif, address, initialBalance)
+	newBalance, err := wait.ForBalanceChange(ctx, l1Client, address, initialBalance)
 	if withSmallSequencerWindow {
 		// Verify that Alice's balance decreases as expected.
 		require.NoError(t, err, "Failed to get new balance")
