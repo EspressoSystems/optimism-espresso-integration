e1d46c5efc94e37c8368a170fb67d08444194a13
Keyao Shen <shenkeyao@gmail.com>
2025-05-27 15:02:34 -0700

    Rename LaunchDecaffNode to LaunchCaffNode (#158)

--- espresso/environment/3_3_fast_derivation_and_caff_node_test.go
+++ espresso/environment/3_3_fast_derivation_and_caff_node_test.go
@@ -66,7 +66,7 @@ func TestFastDerivationAndCaffNode(t *testing.T) {
 	defer env.Stop(t, system)
 	defer env.Stop(t, espressoDevNode)
 
-	caffNode, err := env.LaunchDecaffNode(t, system, espressoDevNode)
+	caffNode, err := env.LaunchCaffNode(t, system, espressoDevNode)
 	if have, want := err, error(nil); have != want {
 		t.Fatalf("failed to start caff node:\nhave:\n\t\"%v\"\nwant:\n\t\"%v\"\n", have, want)
 	}
