e04c86b14e5dd11ae64ca397866578285739adf8
Keyao Shen <shenkeyao@gmail.com>
2025-05-27 15:02:34 -0700

    Rename LaunchDecaffNode to LaunchCaffNode (#158)

--- espresso/environment/7_stateless_batcher_test.go
+++ espresso/environment/7_stateless_batcher_test.go
@@ -2,12 +2,13 @@ package environment_test
 
 import (
 	"context"
-	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/geth"
-	"github.com/ethereum/go-ethereum/common"
 	"math/big"
 	"math/rand/v2"
 	"testing"
 	"time"
+
+	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/geth"
+	"github.com/ethereum/go-ethereum/common"
 
 	env "github.com/ethereum-optimism/optimism/espresso/environment"
 	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/wait"
@@ -50,7 +51,7 @@ func TestStatelessBatcher(t *testing.T) {
 	defer env.Stop(t, system)
 	defer env.Stop(t, espressoDevNode)
 
-	caffNode, err := env.LaunchDecaffNode(t, system, espressoDevNode)
+	caffNode, err := env.LaunchCaffNode(t, system, espressoDevNode)
 	if have, want := err, error(nil); have != want {
 		t.Fatalf("failed to start caff node:\nhave:\n\t\"%v\"\nwant:\n\t\"%v\"\n", have, want)
 	}
