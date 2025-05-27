3a7eb16f3b02cc4ac85b7aff0197b2f0b58f7b3c
Keyao Shen <shenkeyao@gmail.com>
2025-05-27 15:02:34 -0700

    Rename LaunchDecaffNode to LaunchCaffNode (#158)

--- espresso/environment/3_2_espresso_deterministic_state_test.go
+++ espresso/environment/3_2_espresso_deterministic_state_test.go
@@ -59,7 +59,7 @@ func TestDeterministicDerivationExecutionStateWithInvalidTransaction(t *testing.
 	defer env.Stop(t, system)
 	defer env.Stop(t, espressoDevNode)
 
-	caffNode, err := env.LaunchDecaffNode(t, system, espressoDevNode)
+	caffNode, err := env.LaunchCaffNode(t, system, espressoDevNode)
 	if have, want := err, error(nil); have != want {
 		t.Fatalf("failed to start caff node:\nhave:\n\t\"%v\"\nwant:\n\t\"%v\"\n", have, want)
 	}
@@ -272,7 +272,7 @@ func TestValidEspressoTransactionCreation(t *testing.T) {
 	defer env.Stop(t, system)
 	defer env.Stop(t, espressoDevNode)
 
-	caffNode, err := env.LaunchDecaffNode(t, system, espressoDevNode)
+	caffNode, err := env.LaunchCaffNode(t, system, espressoDevNode)
 	if have, want := err, error(nil); have != want {
 		t.Fatalf("failed to start caff node:\nhave:\n\t\"%v\"\nwant:\n\t\"%v\"\n", have, want)
 	}
