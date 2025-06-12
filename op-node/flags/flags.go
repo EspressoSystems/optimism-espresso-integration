9228ed534f6b07e948dfca49942c420716fdb41e
Keyao Shen <shenkeyao@gmail.com>
2025-06-12 16:55:39 -0700

    Provide docker for deployment  (#167)
    * Add docker compose and update dockerfile
    
    * Add env, remove ports
    
    * Remove optional script
    
    * Fix build
    
    * Fix in progress
    
    * Fix services
    
    * Fix l1 connection
    
    * Add hash check, remove auto generated file
    
    * Move to espresso dir
    
    * Add genesis file
    
    * Fix chain ID
    
    * Add op-geth, fix hashes, remove l1 genesis file.
    
    * Fix op geth, update hashes
    
    * Fix caff node errors
    
    * Commit jwt file as well
    
    * Remove jwt
    
    * Add L1 genesis file
    
    * Add back l2 genesis file but rename it
    
    * Update gitignore, remove unnecessary commands
    
    * Update readme
    
    * Update readme

--- op-node/flags/flags.go
+++ op-node/flags/flags.go
@@ -521,6 +521,8 @@ var optionalFlags = []cli.Flag{
 	CaffNodeNextHotShotBlockNum,
 	CaffNodePollingHotShotPollingInterval,
 	CaffNodeHotShotUrls,
+	CaffNodeEspressoLightClientAddr,
+	CaffNodeL1EthRpc,
 }
 
 var DeprecatedFlags = []cli.Flag{
