9e84f8884dce41697253cca2d45387999b2d3757
Philippe Camacho <philippe@espressosys.com>
2025-07-01 12:42:57 -0400

    Example of OP stack contracts deployment that works.

--- espresso/deploy_op_contracts.sh
+++ espresso/deploy_op_contracts.sh
@@ -24,15 +24,15 @@ just build
 cd $ROOT_DIR/espresso
 
 
-echo "\nDeploying superchain contracts..."
-op-deployer bootstrap superchain \
---l1-rpc-url="$L1_RPC_URL" \
---private-key="$OPERATOR_PRIVATE_KEY" \
---artifacts-locator="file://$ARTIFACTS_LOCATOR_DIR" \
---outfile=$BOOTSTRAP_SUPERCHAIN_OUTPUT \
---superchain-proxy-admin-owner="$OPERATOR_ADDRESS" \
---protocol-versions-owner="$OPERATOR_ADDRESS" \
---guardian="$OPERATOR_ADDRESS"
+#echo "\nDeploying superchain contracts..."
+#op-deployer bootstrap superchain \
+#--l1-rpc-url="$L1_RPC_URL" \
+#--private-key="$OPERATOR_PRIVATE_KEY" \
+#--artifacts-locator="file://$ARTIFACTS_LOCATOR_DIR" \
+#--outfile=$BOOTSTRAP_SUPERCHAIN_OUTPUT \
+#--superchain-proxy-admin-owner="$OPERATOR_ADDRESS" \
+#--protocol-versions-owner="$OPERATOR_ADDRESS" \
+#--guardian="$OPERATOR_ADDRESS"
 
 
 PROXY_ADMIN_ADDRESS=`jq -r '.proxyAdminAddress' $BOOTSTRAP_SUPERCHAIN_OUTPUT`
@@ -40,24 +40,24 @@ SUPERCHAIN_CONFIG_PROXY_ADDRESS=`jq -r '.superchainConfigProxyAddress' $BOOTSTRA
 PROTOCOL_VERSIONS_PROXY_ADDRESS=`jq -r '.protocolVersionsProxyAddress' $BOOTSTRAP_SUPERCHAIN_OUTPUT`
 
 
-echo "\nBootstraping implementations contracts..."
-op-deployer bootstrap implementations \
---artifacts-locator="file://$ARTIFACTS_LOCATOR_DIR" \
---l1-rpc-url="$L1_RPC_URL" \
---outfile="$BOOTSTRAP_IMPLEMENTATION_OUTPUT" \
---mips-version="1" \
---private-key="$OPERATOR_PRIVATE_KEY" \
---superchain-config-proxy="$SUPERCHAIN_CONFIG_PROXY_ADDRESS" \
---protocol-versions-proxy="$PROTOCOL_VERSIONS_PROXY_ADDRESS" \
---upgrade-controller="$OPERATOR_ADDRESS"
+#echo "\nBootstraping implementations contracts..."
+#op-deployer bootstrap implementations \
+#--artifacts-locator="file://$ARTIFACTS_LOCATOR_DIR" \
+#--l1-rpc-url="$L1_RPC_URL" \
+#--outfile="$BOOTSTRAP_IMPLEMENTATION_OUTPUT" \
+#--mips-version="1" \
+#--private-key="$OPERATOR_PRIVATE_KEY" \
+#--superchain-config-proxy="$SUPERCHAIN_CONFIG_PROXY_ADDRESS" \
+#--protocol-versions-proxy="$PROTOCOL_VERSIONS_PROXY_ADDRESS" \
+#--upgrade-controller="$OPERATOR_ADDRESS"
 
 
-echo "\nInitializing the intents..."
-op-deployer init \
---l1-chain-id $L1_CHAIN_ID \
---l2-chain-ids 1 \
---outdir $DEPLOYMENT_DIR \
---intent-type standard
+#echo "\nInitializing the intents..."
+#op-deployer init \
+#--l1-chain-id $L1_CHAIN_ID \
+#--l2-chain-ids 1 \
+#--outdir $DEPLOYMENT_DIR \
+#--intent-type standard-overrides
 
 
 echo "Deploy the contracts for real!"
