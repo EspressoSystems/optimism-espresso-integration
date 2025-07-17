9e650a0fd800597f628af943fb7a0607ccfc2c40
Artemii Gerasimovich <artemii@espressosys.com>
2025-07-17 15:31:51 +0200

    Enclave-tools (#187)
    Co-authored-by: Philippe Camacho <philippe@espressosys.com>

---
+++ op-batcher/enclave-tools/enclave-tools.go
@@ -0,0 +1,113 @@
+package enclave_tools
+
+import (
+	"context"
+	"crypto/ecdsa"
+	_ "embed"
+	"fmt"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/ethereum-optimism/optimism/espresso/environment"
+	"github.com/ethereum-optimism/optimism/op-batcher/bindings"
+	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/geth"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+type EnclaveMeasurements struct {
+	PCR0 string `json:"PCR0"`
+	PCR1 string `json:"PCR1"`
+	PCR2 string `json:"PCR2"`
+}
+
+// Builds docker and enclaver EIF image for op-batcher and registers EIF's PCR0 with
+// EspressoNitroTEEVerifier. args... are command-line arguments to op-batcher
+// to be baked into the image.
+func BuildBatcherImage(ctx context.Context, opRoot string, tag string, args ...string) (EnclaveMeasurements, error) {
+	intermediateTag := tag + "intermediate"
+
+	dockerCli := new(environment.DockerCli)
+	err := dockerCli.Build(
+		ctx,
+		intermediateTag,
+		filepath.Join(opRoot, "ops/docker/op-stack-go/Dockerfile"),
+		"op-batcher-enclave-target",
+		opRoot,
+		environment.DockerBuildArg{
+			Name:  "ENCLAVE_BATCHER_ARGS",
+			Value: strings.Join(args, " "),
+		},
+	)
+	if err != nil {
+		return EnclaveMeasurements{}, fmt.Errorf("failed to build intermediate docker image: %w", err)
+	}
+
+	// Build EIF image based on the docker image we just built
+	enclaverCli := new(EnclaverCli)
+	manifest := DefaultManifest("op-batcher", tag, intermediateTag)
+	measurements, err := enclaverCli.BuildEnclave(ctx, manifest)
+	return measurements, err
+}
+
+// RegisterEnclaveHash registers the enclave PCR0 hash with the EspressoNitroTEEVerifier.
+func RegisterEnclaveHash(ctx context.Context, authenticatorAddress common.Address, L1Url string, key *ecdsa.PrivateKey, pcr0Bytes []byte) error {
+	l1Client, err := ethclient.DialContext(ctx, L1Url)
+	if err != nil {
+		return fmt.Errorf("failed to connect to L1 client: %w", err)
+	}
+
+	ChainId, err := l1Client.ChainID(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get chain ID: %w", err)
+	}
+
+	authenticator, err := bindings.NewBatchAuthenticator(authenticatorAddress, l1Client)
+	if err != nil {
+		return fmt.Errorf("failed to create batch authenticator: %w", err)
+	}
+
+	verifierAddress, err := authenticator.EspressoTEEVerifier(&bind.CallOpts{})
+	if err != nil {
+		return fmt.Errorf("failed to get verifier address: %w", err)
+	}
+
+	verifier, err := bindings.NewEspressoTEEVerifier(verifierAddress, l1Client)
+	if err != nil {
+		return fmt.Errorf("failed to create verifier: %w", err)
+	}
+
+	nitroVerifierAddress, err := verifier.EspressoNitroTEEVerifier(&bind.CallOpts{})
+	if err != nil {
+		return fmt.Errorf("failed to get nitro verifier address: %w", err)
+	}
+
+	nitroVerifier, err := bindings.NewEspressoNitroTEEVerifier(nitroVerifierAddress, l1Client)
+	if err != nil {
+		return fmt.Errorf("failed to create nitro verifier: %w", err)
+	}
+
+	opts, err := bind.NewKeyedTransactorWithChainID(key, ChainId)
+	if err != nil {
+		return fmt.Errorf("failed to create transactor: %w", err)
+	}
+	registrationTx, err := nitroVerifier.SetEnclaveHash(opts, crypto.Keccak256Hash(pcr0Bytes), true)
+	if err != nil {
+		return fmt.Errorf("failed to create registration transaction: %w", err)
+	}
+
+	receipt, err := geth.WaitForTransaction(registrationTx.Hash(), l1Client, 2*time.Minute)
+	if err != nil {
+		return fmt.Errorf("failed to wait for registration transaction: %w", err)
+	}
+
+	if receipt.Status != types.ReceiptStatusSuccessful {
+		return fmt.Errorf("registration transaction failed")
+	}
+
+	return nil
+}
