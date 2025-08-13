845cbd370edf19e87af8aec82b9b07c6c927964e
Keyao Shen <shenkeyao@gmail.com>
2025-08-13 14:43:56 -0700

    IA1.2.5 Generate dockers for the Caff node, OP deployer, and OP batcher services (#203)
    * Working devnet
    
    * Update dockers for l1 services
    
    * Fix op-proposer
    
    * Fix node js script
    
    * Remove npm from script
    
    * Install foundry
    
    * Add op-deployer path
    
    * Install dasel
    
    * Fix reshape-allocs script
    
    * Continue fixing reshape-allocs script
    
    * Continue fixing reshape-allocs script
    
    * Convert branch name
    
    * Undo branch name change
    
    * Fix typo
    
    * Fix dockerfile for l1-genesis
    
    * Update dockerfile for op-geth
    
    * More conflicts
    
    * More conflicts
    
    * Fix l2-genesis
    
    * Fix context path
    
    * Fix deployer for terraform
    
    * Fix path
    
    * More fixes and rebuild
    
    * Add preparation scripts to l2 CI
    
    * Fix l1 rpc path
    
    * Move l2-rollup command to dockerfile
    
    * Add config path in docker for op-node
    
    * Add preparation steps to op-node CI
    
    * Add l2-config to CI
    
    * Force jwt
    
    * Remove newline
    
    * Add image for batcher
    
    * Add CI for batcher
    
    * Restore servcie order
    
    ---------
    
    Co-authored-by: Artemii Gerasimovich <artemii@espressosys.com>

--- espresso/docker/op-geth/op-geth-init.sh
+++ espresso/docker/op-geth/op-geth-init.sh
@@ -1,23 +1,77 @@
 #!/bin/sh
-set -e
+set -euo pipefail
 
-# Set the default ports if not provided.
+# Set default values.
+L1_HTTP_PORT=${L1_HTTP_PORT:-8545}
 OP_HTTP_PORT=${OP_HTTP_PORT:-8546}
-OP_ENGINE_PORT=$${OP_ENGINE_PORT:-8552}
+OP_ENGINE_PORT=${OP_ENGINE_PORT:-8552}
+L2_CHAIN_ID=${L2_CHAIN_ID:-22266222}
 
-# Initialize database if not already done.
-if [ ! -f /data/geth/chaindata/CURRENT ]; then
-  echo "Initializing op-geth database..."
-  geth init --datadir=/data --state.scheme=path /l2-genesis-devnet.json
-  echo "op-geth initialization completed"
-else
-  echo "op-geth database already initialized, skipping..."
-fi
+# Mode can be "genesis", "rollup", or "geth" (default).
+MODE=${MODE:-geth}
 
-# Start op-geth with the specified configuration
-exec geth \
-    --datadir=/data \
-    --networkid=1 \
+if [ "$MODE" = "genesis" ]; then
+  echo "=== Running L2 Genesis Mode ==="
+
+  echo "Generating genesis..."
+  op-deployer inspect genesis --workdir /deployer --outfile /config/genesis.json $L2_CHAIN_ID
+
+  echo "Updating genesis timestamp..."
+  dasel put -f /config/genesis.json -s .timestamp -v $(printf '0x%x\n' $(date +%s))
+
+  if [[ ! -f /config/jwt.txt ]]; then
+      echo "Generating JWT token..."
+      # TODO (Keyao) Use a random value?
+      printf "2692310708e4207ecd73bf5597a59ab9cd085380108a7787b3d6be22840e37f0" > /config/jwt.txt
+  fi
+
+  echo "Waiting for L1 finalized block..."
+  while true; do
+    finalized_block=$(curl -s -X POST -H "Content-Type: application/json" \
+      --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["finalized", false],"id":1}' \
+      "$L1_RPC" | jq -r '.result.number')
+
+    if [[ -z "$$finalized_block" || "$$finalized_block" == "null" ]]; then
+      echo "No finalized block yet, waiting..."
+      sleep 3
+      continue
+    fi
+
+    echo "Found L1 finalized block, exiting"
+    break
+  done
+  echo "L2 genesis setup complete"
+    exit 0
+
+elif [ "$MODE" = "geth" ]; then
+  echo "=== Starting OP Geth Mode ==="
+
+  # Wait for genesis.json to be available.
+  while [[ ! -f "/config/genesis.json" ]]; do
+      echo "Waiting for genesis.json to be generated..."
+      sleep 2
+  done
+
+  # Wait for JWT secret to be available.
+  while [[ ! -f "/config/jwt.txt" ]]; do
+      echo "Waiting for JWT secret to be generated..."
+      sleep 2
+  done
+
+  # Initialize database if not already done.
+  if [ ! -d "/data/geth" ]; then
+      echo "Initializing OP Geth database..."
+      geth --gcmode=archive init --state.scheme=hash --datadir=/data/geth /config/genesis.json
+      echo "OP Geth initialization completed"
+  else
+      echo "OP Geth database already initialized, skipping..."
+  fi
+
+  # Start OP Geth with the specified configuration.
+  echo "Starting OP Geth..."
+  exec geth \
+    --datadir=/data/geth \
+    --networkid=${L2_CHAIN_ID} \
     --http \
     --http.addr=0.0.0.0 \
     --http.port=${OP_HTTP_PORT} \
@@ -31,3 +85,38 @@ exec geth \
     --rollup.disabletxpoolgossip=true \
     --rollup.halt=major \
     --nodiscover
+
+elif [ "$MODE" = "rollup" ]; then
+  echo "=== Running L2 Rollup Config Mode ==="
+
+  echo "Generating rollup config..."
+  op-deployer inspect rollup --workdir /deployer --outfile /config/rollup.json $L2_CHAIN_ID
+
+  echo "Updating L1 genesis info..."
+  L1_HASH=$(curl -X POST \
+          "${L1_RPC}" \
+          -H 'Content-Type: application/json' \
+          -d '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["0x0", false],"id":1}' \
+          | jq -r ".result.hash")
+  dasel put -f /config/rollup.json -s .genesis.l1.hash -t string -v $L1_HASH
+  dasel put -f /config/rollup.json -s .genesis.l1.number -t int -v 0
+
+  echo "Updating L2 genesis info..."
+  L2_HASH=$(curl -X POST \
+          "${OP_RPC}" \
+          -H 'Content-Type: application/json' \
+          -d '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["0x0", false],"id":1}' \
+          | jq -r ".result.hash")
+  dasel put -f /config/rollup.json -s .genesis.l2.hash -t string -v $L2_HASH
+  dasel put -f /config/rollup.json -s .genesis.l2.number -t int -v 0
+
+  echo "Updating rollup l2_time..."
+  dasel put -f /config/rollup.json -s .genesis.l2_time -t int -v $(date +%s)
+
+  echo "L2 rollup config complete"
+  exit 0
+
+else
+    echo "Unknown MODE: $MODE. Use 'genesis' or 'geth'"
+    exit 1
+fi
