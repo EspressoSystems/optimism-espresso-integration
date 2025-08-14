23fd979c99fd7c9d8f0bd055f5f9bce16837af62
Jeb Bearer <jeb@espressosys.com>
2025-08-14 15:24:19 -0700

    Remove unnecessary sleep

--- espresso/devnet-tests/batcher_restart_test.go
+++ espresso/devnet-tests/batcher_restart_test.go
@@ -36,11 +36,6 @@ func TestBatcherRestart(t *testing.T) {
 	// Bring the batcher back up and check that it processes the transaction which was submitted
 	// while it was down.
 	require.NoError(t, d.ServiceUp("op-batcher"))
-	// TODO this delay shouldn't really be necessary. It just takes the verifier a little while to
-	// receive the transaction after the batcher starts up; a few seconds longer than the time limit
-	// that is built into `VerifySimpleL2Burn`. This can likely be fixed by changing some
-	// configuration, such as how quickly the batcher posts or the L1 block or epoch time.
-	d.SleepRecoveryDuration()
 	require.NoError(t, d.VerifySimpleL2Burn(receipt))
 
 	// Submit another transaction at the end just to check that things stay working.
