59526653e51efb213e0c4101b03b807756f8e3e1
Jeb Bearer <jeb.bearer@gmail.com>
2025-08-18 19:35:49 -0700

    TA1: Add devnet test for batcher restart (#204)
    * Add devnet test for batcher restart
    
    * Check error returns
    
    * Separate op-geth instances for each L2 node
    
    * Build devnet dockers in CI
    
    * Build op-deployer in CI
    
    * Try larger runner
    
    * Increase test outage and recovery time
    
    * Try to speed up transaction verification
    
    * Do not drop batches before we have seen a finalized L1 block
    
    * Remove unnecessary sleep
    
    * Build containers in dependency order
    
    * Don't copy config file into Docker image at build time
    
    * Checkout submodules in CI
    
    * Don't copy config file into Docker image at build time
    
    * Run devnet test in separate workflow

---
+++ espresso/devnet-tests/batcher_restart_test.go
@@ -0,0 +1,44 @@
+package devnet_tests
+
+import (
+	"context"
+	"testing"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBatcherRestart(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	d := NewDevnet(ctx, t)
+	require.NoError(t, d.Up())
+	defer func() {
+		require.NoError(t, d.Down())
+	}()
+
+	// Send a transaction just to check that everything has started up ok.
+	require.NoError(t, d.RunSimpleL2Burn())
+
+	// Shut down the batcher and have another transaction submitted while it is down.
+	require.NoError(t, d.ServiceDown("op-batcher"))
+	d.SleepOutageDuration()
+
+	receipt, err := d.SubmitSimpleL2Burn()
+	require.NoError(t, err)
+
+	// Check that while the batcher is down, the verifier does NOT process submitted transactions.
+	d.SleepOutageDuration()
+	_, err = d.L2Verif.TransactionReceipt(ctx, receipt.Receipt.TxHash)
+	require.ErrorIs(t, err, ethereum.NotFound)
+
+	// Bring the batcher back up and check that it processes the transaction which was submitted
+	// while it was down.
+	require.NoError(t, d.ServiceUp("op-batcher"))
+	require.NoError(t, d.VerifySimpleL2Burn(receipt))
+
+	// Submit another transaction at the end just to check that things stay working.
+	d.SleepRecoveryDuration()
+	require.NoError(t, d.RunSimpleL2Burn())
+}
