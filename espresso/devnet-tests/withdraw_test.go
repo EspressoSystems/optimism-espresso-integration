a7564e021f7fef2eaaab3ef18ade0a561f9780b0
Philippe Camacho <philippe@espressosys.com>
2025-08-27 16:36:49 -0400

    Basic test running in devnet environment.

---
+++ espresso/devnet-tests/withdraw_test.go
@@ -0,0 +1,23 @@
+package devnet_tests
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestWithdraw(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	d := NewDevnet(ctx, t)
+	require.NoError(t, d.Up())
+	defer func() {
+		require.NoError(t, d.Down())
+	}()
+
+	// Send a transaction just to check that everything has started up ok.
+	require.NoError(t, d.RunSimpleL2Burn())
+
+}
