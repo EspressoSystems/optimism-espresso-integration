c079d3da9f29e96800a581251dd0c110701f8407
Jeb Bearer <jeb.bearer@gmail.com>
2025-09-12 13:49:53 -0700

    Test a challenge game in the docker devnet (#228)
    * Add test for devnet challenge game
    
    * Fixes
    
    * Make sure batcher key used by default in the devnet tests is the
      same one registered in the inbox contract
    * Remove check in batcher that prevents it from sending transactions
      to Espresso immediately
    
    * Ensures that the deployment files are deleted before building a new devnet. Update README_ESPRESSO.md to remind running docker as a non root user.
    
    * Run devnet tests on CI again.
    
    * Ensure deployment files are not written by the root user.
    
    * Ignore rotate batcher key and change batch inbox owmer tests.
    
    * Clean way of setting UID and GID.
    
    * Ignore devnet tests for now so that we can merge.
    
    * Add fallback values for UID and GID.
    
    * Pinpoint forge version in CI as the linter is complaining.
    
    * Add comment regarding the number of claims.
    
    * Add comment to function TaggedWriter.Write.
    
    ---------
    
    Co-authored-by: Philippe Camacho <philippe@espressosys.com>

---
+++ espresso/devnet-tests/challenge_test.go
@@ -0,0 +1,50 @@
+package devnet_tests
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestChallengeGame(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	d := NewDevnet(ctx, t)
+	require.NoError(t, d.Up())
+	defer func() {
+		require.NoError(t, d.Down())
+	}()
+
+	// Wait for the proposer to make a claim.
+	var games []ChallengeGame
+	for len(games) == 0 {
+		var err error
+		t.Logf("waiting for a challenge game")
+		time.Sleep(5 * time.Second)
+		games, err = d.ListChallengeGames()
+		require.NoError(t, err)
+	}
+	t.Logf("game created: %v", games[0])
+	require.Equal(t, uint64(1), games[0].Claims)
+
+	// Attack the first claimed state.
+	t.Logf("attacking game")
+	require.NoError(t, d.OpChallenger("move", "--attack", "--game-address", games[0].Address.Hex()))
+
+	// Check that the proposer correctly responds.
+	CLAIMS_NUMBER := uint64(3) // First claim by the proposer + attack + response
+	for {
+		updatedGames, err := d.ListChallengeGames()
+		require.NoError(t, err)
+		if updatedGames[0].Claims == CLAIMS_NUMBER {
+			require.Equal(t, updatedGames[0].OutputRoot, games[0].OutputRoot)
+			break
+		}
+
+		t.Logf("waiting for a response")
+		time.Sleep(time.Second)
+	}
+}
