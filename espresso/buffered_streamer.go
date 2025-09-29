f4f697155b8b9b4a7c2d20b13f70a4779205c331
Theodore Schnepper <tschnepper@espressosys.com>
2025-09-29 11:03:59 -0600

    Add Unit Tests for BufferedEspressoStreamer
    Fix BufferedEspressoStreamer behavior
    
    While adding unit tests for the BufferedEspressoStreamer it was noticed that the
    position of the L2 and the L1 for the Buffer were being mixed together at times.  This
    would ultimately lead to very difficult to detect bugs based on observed behavior
    alone.
    
    With the addition of the unit tests identifying the issue, the buffer adjustment
    behavior has been adjusted to apply to the L2 position in isolation away from the
    L1 positions.  The L1 positions will cause a larger Reset in the underlying logic.

--- espresso/buffered_streamer.go
+++ espresso/buffered_streamer.go
@@ -31,7 +31,8 @@ type BufferedEspressoStreamer[B Batch] struct {
 	// local offset
 	readPos uint64
 
-	startingBatchPos uint64
+	startingBatchPos    uint64
+	currentSafeL1Origin eth.BlockID
 }
 
 // Compile time assertion to ensure BufferedEspressoStreamer implements
@@ -50,33 +51,38 @@ func (b *BufferedEspressoStreamer[B]) Update(ctx context.Context) error {
 	return b.streamer.Update(ctx)
 }
 
-// RefreshSafeL1Origin updates the safe L1 origin for the buffered streamer.
-// This method attempts to safely handle the adjustment of the safeL1Origin
-// without needing to defer to the underlying streamer unless necessary.
-func (b *BufferedEspressoStreamer[B]) RefreshSafeL1Origin(safeL1Origin eth.BlockID) error {
-	if safeL1Origin.Number < b.startingBatchPos {
-		// If the safeL1Origin is before the starting batch position, we need to
-		// reset the buffered streamer to ensure we don't miss any batches.
-
+// handleL2PositionUpdate handles the update of the L2 position for the
+// buffered streamer.
+//
+// There are three conditions to consider:
+//  1. If the next position is before the starting batch position, we need to
+//     reset the underlying streamer, and dump our local buffer, as this
+//     indicates a need to move backwards before our earliest known batch.
+//  2. If the next position is after our starting batch position, then we
+//     can drop all earlier stored batches in our buffer, and adjust our
+//     read position accordingly.  This should appear to the consumer as nothing
+//     has changed progression-wise, but it allows us to reclaim memory.
+//  3. If the next position is the same as our starting batch position, then
+//     we do nothing, as we are already at the correct position.
+func (b *BufferedEspressoStreamer[B]) handleL2PositionUpdate(nextPosition uint64) {
+	if nextPosition < b.startingBatchPos {
+		// If the next position is before the starting batch position,
+		// we need to reset the buffered streamer to ensure we don't
+		// miss any batches.
 		b.readPos = 0
-		b.startingBatchPos = safeL1Origin.Number
+		b.startingBatchPos = nextPosition
 		b.batches = make([]*B, 0)
-		if cast, castOk := b.streamer.(interface{ RefreshSafeL1Origin(eth.BlockID) error }); castOk {
-			// If the underlying streamer has a method to refresh the safe L1 origin,
-			// we call it to ensure it is aware of the new safe L1 origin.
-			return cast.RefreshSafeL1Origin(safeL1Origin)
-		}
-		return nil
+		b.streamer.Reset()
+		return
 	}
 
-	if safeL1Origin.Number > b.startingBatchPos {
+	if nextPosition > b.startingBatchPos {
 		// We want to advance the read position, and we are indicating that
 		// we no longer will need to refer to older batches.  So instead, we
 		// will want to adjust the buffer, and read position based on the
-		// new safeL1Origin.
+		// new nextPosition.
 
-		nextSafePosition := safeL1Origin.Number
-		positionAdjustment := nextSafePosition - b.startingBatchPos
+		positionAdjustment := nextPosition - b.startingBatchPos
 		if positionAdjustment <= uint64(len(b.batches)) {
 			// If the adjustment is within the bounds of the current buffer,
 			// we can simply adjust the read position and starting batch position.
@@ -86,15 +92,37 @@ func (b *BufferedEspressoStreamer[B]) RefreshSafeL1Origin(safeL1Origin eth.Block
 			b.batches = make([]*B, 0)
 			b.readPos = 0
 		}
-		b.startingBatchPos = nextSafePosition
+		b.startingBatchPos = nextPosition
+		return
+	}
+}
+
+// RefreshSafeL1Origin updates the safe L1 origin for the buffered streamer.
+// This method attempts to safely handle the adjustment of the safeL1Origin
+// without needing to defer to the underlying streamer unless necessary.
+func (b *BufferedEspressoStreamer[B]) RefreshSafeL1Origin(safeL1Origin eth.BlockID) error {
+	if safeL1Origin.Number < b.currentSafeL1Origin.Number {
+		// If the safeL1Origin is before the starting batch position, we need to
+		// reset the buffered streamer to ensure we don't miss any batches.
+		b.currentSafeL1Origin = safeL1Origin
+		b.startingBatchPos = 0
+		b.readPos = 0
+		b.batches = make([]*B, 0)
+		if cast, castOk := b.streamer.(interface{ RefreshSafeL1Origin(eth.BlockID) error }); castOk {
+			// If the underlying streamer has a method to refresh the safe L1 origin,
+			// we call it to ensure it is aware of the new safe L1 origin.
+			return cast.RefreshSafeL1Origin(safeL1Origin)
+		}
 		return nil
 	}
 
+	b.currentSafeL1Origin = safeL1Origin
 	return nil
 }
 
 // Refresh implements EspressoStreamerIFace
 func (b *BufferedEspressoStreamer[B]) Refresh(ctx context.Context, finalizedL1 eth.L1BlockRef, safeBatchNumber uint64, safeL1Origin eth.BlockID) error {
+	b.handleL2PositionUpdate(safeBatchNumber)
 	if err := b.RefreshSafeL1Origin(safeL1Origin); err != nil {
 		return err
 	}
@@ -137,13 +165,23 @@ func (b *BufferedEspressoStreamer[B]) Next(ctx context.Context) *B {
 
 	// If we don't have a batch in the buffer, fetch the next one from the streamer
 	batch := b.streamer.Next(ctx)
-	if batch != nil {
-		b.batches = append(b.batches, batch)
-		b.readPos++
-		return batch
+
+	// No more batches available at the moment
+	if batch == nil {
+		return nil
 	}
 
-	return nil
+	number := (*batch).Number()
+	if number < b.startingBatchPos {
+		// If the batch number is before the starting batch position, we ignore
+		// it, and want to fetch the next one
+		return b.Next(ctx)
+	}
+
+	b.batches = append(b.batches, batch)
+	b.readPos++
+	return batch
+
 }
 
 // UnmarshalBatch implements EspressoStreamerIFace
