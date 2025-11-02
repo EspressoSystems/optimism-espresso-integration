1f48fba69c3922fa5e522efda74e63cccd0b7b56
dailinsubjam <dlsubjam@gmail.com>
2025-11-02 05:27:25 +0000

    Add back forgotten config when rebase celo-sync-14

--- op-node/flags/flags.go
+++ op-node/flags/flags.go
@@ -470,6 +470,13 @@ var (
 		Category: RollupCategory,
 		Hidden:   true,
 	}
+	ExperimentalOPStackAPI = &cli.BoolFlag{
+		Name:     "experimental.sequencer-api",
+		Usage:    "Enables experimental test sequencer RPC functionality",
+		Required: false,
+		EnvVars:  prefixEnvVars("EXPERIMENTAL_SEQUENCER_API"),
+		Category: MiscCategory,
+	}
 )
 
 var requiredFlags = []cli.Flag{
@@ -525,6 +532,7 @@ var optionalFlags = []cli.Flag{
 	InteropJWTSecret,
 	InteropDependencySet,
 	IgnoreMissingPectraBlobSchedule,
+	ExperimentalOPStackAPI,
 }
 
 var DeprecatedFlags = []cli.Flag{
