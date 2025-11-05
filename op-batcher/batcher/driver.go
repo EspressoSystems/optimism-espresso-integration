23b6b63fe08e8c05067894b0a7676c951af4bcaf
Artemii Gerasimovich <artemii@espressosys.com>
2025-11-05 19:13:15 +0100

    Reduce log spam for Espresso tx queue

--- op-batcher/batcher/driver.go
+++ op-batcher/batcher/driver.go
@@ -1011,7 +1011,7 @@ func (l *BatchSubmitter) sendTx(txdata txData, isCancel bool, candidate *txmgr.T
 			},
 		)
 		if !goroutineSpawned {
-			log.Warn("failed to spawn Espresso tx goroutine")
+			log.Trace("failed to spawn Espresso tx goroutine")
 			l.recordFailedDARequest(txdata.ID(), nil)
 		}
 		return
