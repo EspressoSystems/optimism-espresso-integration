a9e52630ecd05b1cf4c73685a2e78be082f51972
Philippe Camacho <philippe@espressosys.com>
2025-11-15 13:06:23 -0300

    Ensure the devnet uses two different addresses for the TEE and non TEE batchers.

--- espresso/scripts/prepare-allocs.sh
+++ espresso/scripts/prepare-allocs.sh
@@ -73,7 +73,9 @@ op-deployer init --l1-chain-id "${L1_CHAIN_ID}" \
                  --outdir ${DEPLOYER_DIR}
 
 dasel put -f "${DEPLOYER_DIR}/intent.toml" -s .chains.[0].espressoEnabled -t bool -v true
-dasel put -f "${DEPLOYER_DIR}/intent.toml" -s .chains.[0].nonTeeBatcher -v "${OPERATOR_ADDRESS}"
+# Use a distinct address for the non-TEE batcher to satisfy the BatchInbox constructor
+# In devnet we reuse the proposer address for the non-TEE batcher role.
+dasel put -f "${DEPLOYER_DIR}/intent.toml" -s .chains.[0].nonTeeBatcher -v "${PROPOSER_ADDRESS}"
 dasel put -f "${DEPLOYER_DIR}/intent.toml" -s .l1ContractsLocator -v "${ARTIFACTS_DIR}"
 dasel put -f "${DEPLOYER_DIR}/intent.toml" -s .l2ContractsLocator -v "${ARTIFACTS_DIR}"
 dasel put -f "${DEPLOYER_DIR}/intent.toml" -s .opcmAddress -v `jq -r .opcmAddress < ${DEPLOYER_DIR}/bootstrap_implementations.json`
