6236602ab9ef983f7d8a584634b734d9d138d879
Keyao Shen <shenkeyao@gmail.com>
2025-11-14 16:27:30 -0800

    Add missed file

--- espresso/devnet-tests/batcher_restart_test.go
+++ espresso/devnet-tests/batcher_restart_test.go
@@ -13,7 +13,7 @@ func TestBatcherRestart(t *testing.T) {
 	defer cancel()
 
 	d := NewDevnet(ctx, t)
-	require.NoError(t, d.Up(false))
+	require.NoError(t, d.Up(NON_TEE))
 	defer func() {
 		require.NoError(t, d.Down())
 	}()
