06e9a187ad8448eaedf0ffe1bd8f828b807bcbb8
Sishan Long <dlsubjam@gmail.com>
2025-11-18 09:20:23 -0800

    Create enclave ami for enclave test (#277)
    * create enclave ami
    
    * use new ami and restore the ci workflow and update enclave prepare ami script
    
    * new AMI
    
    * update readme

--- espresso/scripts/enclave-prepare-ami.sh
+++ espresso/scripts/enclave-prepare-ami.sh
@@ -3,24 +3,26 @@ set -euo pipefail
 set -x
 
 echo "[*] Setting up Nix"
-sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon --no-confirm
+sh <(curl --proto '=https' --tlsv1.2 -sSfL https://nixos.org/nix/install) --daemon --yes
 source /etc/profile.d/nix.sh
 nix-env -iA cachix -f https://cachix.org/api/v1/install
 mkdir -p ~/.config/nix
 echo "trusted-users = root ec2-user" | sudo tee -a /etc/nix/nix.conf && sudo pkill nix-daemon
 
-
 echo "[*] Installing dependencies..."
-sudo yum update -y
-sudo yum install -y git docker
-sudo amazon-linux-extras enable aws-nitro-enclaves-cli
-sudo yum install -y aws-nitro-enclaves-cli-1.4.2
+sudo dnf update -y
+sudo dnf install -y git docker gcc
 
+# Nitro Enclaves CLI for Amazon Linux 2023
+sudo dnf install -y aws-nitro-enclaves-cli aws-nitro-enclaves-cli-devel
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG ne ec2-user || true
+sudo usermod -aG docker ec2-user || true
 
-# Workaround due to https://github.com/foundry-rs/foundry/issues/4736
-sudo yum install -y gcc
+# Rust + svm workaround
 curl https://sh.rustup.rs -sSf | sh -s -- -y
-. $HOME/.cargo/env
+. "$HOME/.cargo/env"
 cargo install svm-rs
 svm install 0.8.15
 svm install 0.8.19
