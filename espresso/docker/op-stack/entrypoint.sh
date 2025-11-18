c707eb81ca6463a83f72a0115bdb78d73fcfa070
Sneh Koul <snehkoul1999@gmail.com>
2025-11-18 14:54:25 -0500

    fix the name of deployer factory address

--- espresso/docker/op-stack/entrypoint.sh
+++ espresso/docker/op-stack/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export "${ENV_PREFIX}_GAME_FACTORY_ADDRESS"=$(jq -r '.opChainDeployments[0].disputeGameFactoryProxyAddress' ./deployer/state.json)
+export "${ENV_PREFIX}_GAME_FACTORY_ADDRESS"=$(jq -r '.opChainDeployments[0].DisputeGameFactoryProxy' ./deployer/state.json)
 
 "$@"
