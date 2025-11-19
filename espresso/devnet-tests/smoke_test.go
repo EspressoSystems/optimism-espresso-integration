37d087701f04173df8ff3d6364dd275e3b513cd3
Keyao Shen <shenkeyao@gmail.com>
2025-11-19 10:56:05 -0800

    Add devnet smoke test with TEE (#268)
    * Add devnet smoke test with TEE
    
    * Remove unnecessary extra timeout
    
    * Add consts, remove incorrect setting
    
    * Use consts
    
    * Add missed file

--- espresso/devnet-tests/smoke_test.go
+++ espresso/devnet-tests/smoke_test.go
@@ -8,12 +8,26 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSmoke(t *testing.T) {
+func TestSmokeWithoutTEE(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Minute)
 	defer cancel()
 
 	d := NewDevnet(ctx, t)
-	require.NoError(t, d.Up())
+	require.NoError(t, d.Up(NON_TEE))
+	defer func() {
+		require.NoError(t, d.Down())
+	}()
+
+	// Send a transaction just to check that everything has started up ok.
+	require.NoError(t, d.RunSimpleL2Burn())
+}
+
+func TestSmokeWithTEE(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Minute)
+	defer cancel()
+
+	d := NewDevnet(ctx, t)
+	require.NoError(t, d.Up(TEE))
 	defer func() {
 		require.NoError(t, d.Down())
 	}()
