63ebe1289a94fee12b9a061cc2427cb7361d441f
Sneh Koul <snehkoul1999@gmail.com>
2025-12-08 13:01:35 -0800

    fix contracts

--- packages/contracts-bedrock/src/L1/BatchAuthenticator.sol
+++ packages/contracts-bedrock/src/L1/BatchAuthenticator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity ^0.8.0;
 
 import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
