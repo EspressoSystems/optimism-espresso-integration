0a6733cd9e8eeea79500ae9605723ea48f2b4a7e
Phil <philippe@espressosys.com>
2025-12-26 17:13:50 -0300

    Test fallback mechanism on devnet (#313)

--- espresso/devnet-tests/devnet_tools.go
+++ espresso/devnet-tests/devnet_tools.go
@@ -229,6 +229,36 @@ func (d *Devnet) ServiceRestart(service string) error {
 	return nil
 }
 
+// callBatcherRPC calls a batcher RPC method on a running batcher service
+func (d *Devnet) callBatcherRPC(service, method string) error {
+	cmd := exec.CommandContext(
+		d.ctx,
+		"docker", "compose", "exec", "-T", service,
+		"sh", "-c",
+		fmt.Sprintf("wget -q -O- --header='Content-Type: application/json' --post-data='{\"jsonrpc\":\"2.0\",\"method\":\"%s\",\"params\":[],\"id\":1}' http://localhost:8545", method),
+	)
+	buf := new(bytes.Buffer)
+	cmd.Stdout = buf
+	cmd.Stderr = buf
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to call %s (%w): %s", method, err, buf.String())
+	}
+	log.Info("RPC call successful", "service", service, "method", method, "response", buf.String())
+	return nil
+}
+
+// StartBatcherSubmitting starts batch submission on a running batcher service
+func (d *Devnet) StartBatcherSubmitting(service string) error {
+	log.Info("starting batch submission", "service", service)
+	return d.callBatcherRPC(service, "admin_startBatcher")
+}
+
+// StopBatcherSubmitting stops batch submission on a running batcher service
+func (d *Devnet) StopBatcherSubmitting(service string) error {
+	log.Info("stopping batch submission", "service", service)
+	return d.callBatcherRPC(service, "admin_stopBatcher")
+}
+
 func (d *Devnet) RollupConfig(ctx context.Context) (*rollup.Config, error) {
 	return d.L2SeqRollup.RollupConfig(ctx)
 }
