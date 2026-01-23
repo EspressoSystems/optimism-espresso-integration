12e809d2c41a376b96b38aff12cec5cdba4352b2
dailinsubjam <dlsubjam@gmail.com>
2026-01-23 20:49:06 +0000

    install docker v2

--- espresso/scripts/run-enclave-restart-test-github-actions.sh
+++ espresso/scripts/run-enclave-restart-test-github-actions.sh
@@ -20,6 +20,13 @@ sudo systemctl enable --now docker
 sudo usermod -a -G docker ec2-user
 sudo chown ec2-user /var/run/docker.sock
 
+echo "[*] Installing Docker Compose v2..."
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.24.5/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+docker compose version
+
 echo "[*] Configuring Nitro Enclaves..."
 sudo systemctl stop nitro-enclaves-allocator.service || true
 echo -e '---\nmemory_mib: 4096\ncpu_count: 2' | sudo tee /etc/nitro_enclaves/allocator.yaml
