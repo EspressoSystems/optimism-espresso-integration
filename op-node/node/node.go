celo-org/optimism

op-batcher: Cherry-pick Altda parallel submitted blobs respect strict holocene order (#379)
* fix(batcher): altda parallel submitted blobs respect strict holocene order (#21)

test(e2e): new altda e2e test for concurrent blob submissions to maintain new holocene strict ordering rules

test(batcher): add altda unit tests for unhappy failure cases (except channel timeout)

test(batcher): fix flaky driver tests + speed them up

test(batcher): robustify batcher driver altda tests

fix(batcher): altda concurrent blob responses are reordered to respect holocene strict ordering rules

docs: fix typos and add documentation comments for some batcher public methods

test(op-alt-da): fix MockDAClient.DeleteData decrement semantic

chore(batcher): move channel failover behavior from TxFailed to AltDASubmissionFailed

The failover logic in the failover feature commits was aded on TxFailed, as the separate function AltDASubmissionFailed didn't exist yet. This change makes it much cleaner as a tx having failed cannot lead to a failover given... since that would come from an ethereum issue.

fix(batcher): bug in sendTransaction

chore(test-logger): fix test logger.Crit which wasn't getting flushed

test(batcher): fix altDASetup w new channel config

DaType was added, so needed to change config to use DaTypeAltDA

style: wrap errors

style(damock): dont explicitly initialize to 0

We make use default values instead

docs(batcher-test): document why channel timeout test is left unimplemented

docs(batcher): fix todos in batcher readme

chore: make lint-go-fix

docs(batcher): fix readme typo

* Change test to write a log instead of stdout

---------

Co-authored-by: Samuel Laferriere <samlaf92@gmail.com>

op-node/node/node.go
@@ -436,7 +436,10 @@ func (n *OpNode) initL2(ctx context.Context, cfg *config.Config) error {
 	if cfg.AltDA.Enabled && err != nil {
 		return fmt.Errorf("failed to get altDA config: %w", err)
 	}
-	altDA := altda.NewAltDA(n.log, cfg.AltDA, rpCfg, n.metrics.AltDAMetrics)
+	altDA, err := altda.NewAltDA(n.log, cfg.AltDA, rpCfg, n.metrics.AltDAMetrics)
+	if err != nil {
+		return fmt.Errorf("failed to create altDA: %w", err)
+	}
 	if cfg.SafeDBPath != "" {
 		n.log.Info("Safe head database enabled", "path", cfg.SafeDBPath)
 		safeDB, err := safedb.NewSafeDB(n.log, cfg.SafeDBPath)

EspressoSystems/optimism-espresso-integration

op-node/node/node.go
@@ -583,7 +583,10 @@ func initL2(ctx context.Context, cfg *config.Config, node *OpNode) (*sources.Eng
 	if cfg.AltDA.Enabled && err != nil {
 		return nil, nil, nil, nil, fmt.Errorf("failed to get altDA config: %w", err)
 	}
-	altDA := altda.NewAltDA(node.log, cfg.AltDA, rpCfg, node.metrics.AltDAMetrics)
+	altDA, err := altda.NewAltDA(node.log, cfg.AltDA, rpCfg, node.metrics.AltDAMetrics)
+	if err != nil {
+		return nil, nil, nil, nil, fmt.Errorf("failed to create altDA: %w", err)
+	}
 	var safeDB closableSafeDB
 	if cfg.SafeDBPath != "" {
 		node.log.Info("Safe head database enabled", "path", cfg.SafeDBPath)