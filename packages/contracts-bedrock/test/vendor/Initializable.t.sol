db6795c8068204ee53be07f2bc1cbe78c7a4b3ae
Keyao Shen <shenkeyao@gmail.com>
2026-04-14 18:03:37 -0700

    Fix short and contract tests

--- packages/contracts-bedrock/test/vendor/Initializable.t.sol
+++ packages/contracts-bedrock/test/vendor/Initializable.t.sol
@@ -382,7 +382,7 @@ contract Initializer_Test is CommonTest {
     function test_cannotReinitialize_succeeds() public {
         // Collect exclusions.
         uint256 j;
-        string[] memory excludes = new string[](14);
+        string[] memory excludes = new string[](15);
         // Contract is currently not being deployed as part of the standard deployment script.
         excludes[j++] = "src/L2/OptimismSuperchainERC20.sol";
         // Periphery contracts don't get deployed as part of the standard deployment script.
@@ -407,6 +407,9 @@ contract Initializer_Test is CommonTest {
         excludes[j++] = "src/L2/*";
         excludes[j++] = "src/celo/*";
         excludes[j++] = "src/L1/FeesDepositor.sol";
+        // Espresso: BatchAuthenticator is deployed by a separate Espresso deployment script,
+        // not the standard deployment script.
+        excludes[j++] = "src/L1/BatchAuthenticator.sol";
 
         // Get all contract names in the src directory, minus the excluded contracts.
         string[] memory contractNames = ForgeArtifacts.getContractNames("src/*", excludes);
