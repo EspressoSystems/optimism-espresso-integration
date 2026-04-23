fe5f127226ac9b19ecaf370b6f49797c73c06214
Theodore Schnepper <tschnepper@espressosys.com>
2026-04-23 17:19:00 -0600

    Fix reported issues from Claude and Gemini
    Fixes spelling, style, and idomatic issue caught by Claude and Gemini.

--- op-batcher/batcher/espresso_transaction_submitter_test.go
+++ op-batcher/batcher/espresso_transaction_submitter_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 // TestEspressoTransactionSubmitterDeadlock is a test that is meant to test
-// the underlying conditions in the espressoTransactionsubmitter that may
+// the underlying conditions in the espressoTransactionSubmitter that may
 // result in a deadlock.
 //
 // The basic idea is to intentionally trigger the circumstances that can
@@ -36,44 +36,44 @@ import (
 // future. This will primarily be due to `SubmitTransaction` being modified
 // to longer explicitly block.
 func TestEspressoTransactionSubmitterDeadlock(t *testing.T) {
+	ctx, cancel := context.WithCancel(t.Context())
+	defer cancel()
 	submitter := batcher.NewEspressoTransactionSubmitter(
 		batcher.WithEspressoClient(new(AlwaysFailingEspressoClient)),
+		batcher.WithContext(ctx),
 	)
 
 	submitter.SpawnWorkers(4, 4)
 	submitter.Start()
 
-	binary.LittleEndian.PutUint64(make([]byte, 8), 1)
-
-	for i := range 1024 * 4 {
+	for i := 0; i < 1024*4; i++ {
 		txn := espressoCommon.Transaction{
 			Namespace: 1,
 			Payload:   make([]byte, 8),
 		}
 		binary.LittleEndian.PutUint64(txn.Payload, uint64(i))
 
-		ctx, cancel := context.WithCancel(context.Background())
+		submitCtx, submitCancel := context.WithCancel(context.Background())
 		go (func(cancel context.CancelFunc, txn espressoCommon.Transaction) {
 			submitter.SubmitTransaction(&txn)
 			cancel()
-		})(cancel, txn)
+		})(submitCancel, txn)
 
 		timeoutCtx, timeoutCancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
 
 		select {
-		case <-ctx.Done():
+		case <-submitCtx.Done():
 			// Things progressed without issue.
 			timeoutCancel()
-			cancel()
+			submitCancel()
 			continue
 		case <-timeoutCtx.Done():
 			// The call to SubmitTransaction did not return within the expected
 			// time frame.
 
 			timeoutCancel()
-			cancel()
-			t.Errorf("SubmitTransaction has blocked for longer than 200 milliseconds, on transaction %d, for error: %s\n", i, timeoutCtx.Err())
-			return
+			submitCancel()
+			t.Fatalf("SubmitTransaction has blocked for longer than 200 milliseconds, on transaction %d, for error: %s\n", i, timeoutCtx.Err())
 		}
 	}
 
