bafa44ce277e5c2c82820d4f9adc78a84e14e9e2
Theodore Schnepper <tschnepper@espressosys.com>
2026-04-24 12:04:27 -0600

    Add a test to verify new expected espressoTransactionSubmitter behavior
    With the changes of the `espressoTransactionSubmitter`'s
    `SubmitTransaction` method, we want to illustrate how to effectively
    utilize the `espressoTransactionSubmitter`'s `SubmitMethod` when it
    returns an error that indicates backpressure.
    
    That being that the transaction should be submitted at some time in the
    future, and that a brief rest should occur before attempting again.

---
+++ op-batcher/batcher/espresso_fake_succeeding_submission_test.go
@@ -0,0 +1,88 @@
+package batcher_test
+
+import (
+	"context"
+	"encoding/binary"
+	"encoding/json"
+	"errors"
+	"sync"
+
+	espressoClient "github.com/EspressoSystems/espresso-network/sdks/go/client"
+	tagged_base64 "github.com/EspressoSystems/espresso-network/sdks/go/tagged-base64"
+	"github.com/EspressoSystems/espresso-network/sdks/go/types"
+	common "github.com/EspressoSystems/espresso-network/sdks/go/types/common"
+)
+
+// FakeSubmissionSucceedingEspressoClient is a mock implementation of the
+// EspressoClient for the explicit purposes of submitting transactions, and
+// seeing their response.
+type FakeSubmissionSucceedingEspressoClient struct {
+	sync.RWMutex
+	espressoClient.EspressoClient
+	txns map[string]common.Transaction
+}
+
+// Compile time check to ensure adherence to EspressoClient interface
+var _ espressoClient.EspressoClient = (*FakeSubmissionSucceedingEspressoClient)(nil)
+
+var (
+	ErrNotInitialized      = errors.New("not initialized")
+	ErrHashCannotBeNil     = errors.New("hash cannot be nil")
+	ErrTransactionNotFound = errors.New("transaction not found")
+)
+
+func (c *FakeSubmissionSucceedingEspressoClient) Init() {
+	c.RLock()
+	defer c.RUnlock()
+	c.txns = make(map[string]common.Transaction)
+}
+
+// FetchTransactionByHash simulates fetching a transaction by its hash. it
+// looks up a transaction for the given hash, and returns the transaction
+// if it is found.
+func (c *FakeSubmissionSucceedingEspressoClient) FetchTransactionByHash(ctx context.Context, hash *types.TaggedBase64) (types.TransactionQueryData, error) {
+	c.RLock()
+	defer c.RUnlock()
+	if c.txns == nil {
+		return types.TransactionQueryData{}, ErrNotInitialized
+	}
+
+	if hash == nil {
+		return types.TransactionQueryData{}, ErrHashCannotBeNil
+	}
+
+	txn, found := c.txns[hash.String()]
+	if !found {
+		return types.TransactionQueryData{}, ErrTransactionNotFound
+	}
+
+	// Just to simulate some processing on the transaction
+	height := binary.LittleEndian.Uint64(txn.Payload)
+
+	return types.TransactionQueryData{
+		Transaction: txn,
+		Hash:        hash,
+		Index:       0,
+		Proof:       json.RawMessage{},
+		BlockHash:   nil,
+		BlockHeight: height,
+	}, nil
+}
+
+// SubmitTransaction simulates a successful transaction submission and stores
+// it for future retrieval
+func (c *FakeSubmissionSucceedingEspressoClient) SubmitTransaction(ctx context.Context, tx common.Transaction) (*common.TaggedBase64, error) {
+	c.Lock()
+	defer c.Unlock()
+	if c.txns == nil {
+		return nil, ErrNotInitialized
+	}
+
+	hash, err := tagged_base64.New("TX", tx.Payload)
+	if err != nil {
+		return nil, err
+	}
+
+	c.txns[hash.String()] = tx
+	return hash, nil
+}
